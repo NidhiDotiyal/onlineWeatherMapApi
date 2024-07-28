743f9cec0958ad2c0dc51caae9cf9905162b388e
NidhiDotiyal <dotiyalnidhi843@gmail.com>
2024-07-28 08:44:53 +0530

    Initial commit

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = '7eaeb2071132e97f5db7b9ad580941fb';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
